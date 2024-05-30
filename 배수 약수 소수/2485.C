c3ce9d1c1b99f69cb7f874c6913a36f6b94f24b9
jmlee8939 <jmlee8939@hanmail.net>
2024-05-31 00:03:38 +0900

    2485

--- 배수 약수 소수/2485.C
+++ 배수 약수 소수/2485.C
@@ -17,30 +17,36 @@ int GCD(int A, int B) {
 }
 
 int N;
-int arr[100001];
-int now_num;
+int now_num, start_n, finish_n;
 int prev_num = 0;
-int prev_gcd = 0;
+int prev_gcd = 1;
 int t_diff;
-int t_gcd;
+int output = 0;
 
 
 int main(void) {
     scanf("%d", &N);
     for (int i=0; i<N; i++) {
         scanf("%d", &now_num);
+        //printf("현재 숫자 : %d\n", now_num);
         if (i == 0) {
-            prev_gcd = now_num - prev_num;
-
+            start_n = now_num;
         } else {
             t_diff = now_num - prev_num;
-
-            printf("%d, 시작 \n",t_diff);
+            if (i == 1) {
+                prev_gcd = t_diff;
+            }
+            if (i==N-1) {
+                finish_n = now_num;
+            }
+            //printf("값 차이 : %d, 이전 GCD : %d \n", t_diff, prev_gcd);
             prev_gcd = GCD(t_diff, prev_gcd); 
+            //printf("현재 GCD : %d \n", prev_gcd);
         }
         prev_num = now_num;
     }
-    printf("%d", prev_gcd);
+    output = (finish_n - start_n)/prev_gcd + 1 - N;
+    printf("%d", output);
 }
 
 
